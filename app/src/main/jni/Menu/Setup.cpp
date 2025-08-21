be513326e261d331dc7e7bf5363b16ae97cf3b29
GOHANX1234 <francee397@gmail.com>
2025-08-21 13:45:04 +0530

    Add files via upload

--- app/src/main/jni/Menu/Setup.cpp
+++ app/src/main/jni/Menu/Setup.cpp
@@ -7,19 +7,19 @@
 
 void Init(JNIEnv *env, jobject thiz, jobject ctx, jobject title, jobject subtitle) {
     //Set sub title
-    setText(env, title, OBFUSCATE("<b>Modded by (yourname)</b>"));
+    setText(env, title, OBFUSCATE("<b>Hex-Core</b>"));
 
     //Set sub title
     setText(env, subtitle, OBFUSCATE("<b><marquee><p style=\"font-size:30\">"
-                                     "<p style=\"color:green;\">Modded by LGL</p> | "
-                                     "https://github.com/LGLTeam | Lorem Ipsum is simply dummy text of the printing and typesetting</p>"
+                                     "<p style=\"color:green;\">Modded by Gohan</p> | "
+                                     "Telegram @Gohan52</p>"
                                      "</marquee></b>"));
 
     //Dialog Example
     //setDialog(ctx,env,OBFUSCATE("Title"),OBFUSCATE("Message Example"));
 
     //Toast Example
-    Toast(env, ctx, OBFUSCATE("Modded by YOU"), ToastLength::LENGTH_LONG);
+    Toast(env, ctx, OBFUSCATE("Welcome to Hex-Core"), ToastLength::LENGTH_LONG);
 }
 
 int RegisterMenu(JNIEnv *env) {
